cbd2120b5364a04546843f57a718afc2b0fdb5e7
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-12-20 11:09:38 -0500

    Add todo-reducer for reducer composition

--- src/Todo/todo_reducer.js
+++ src/Todo/todo_reducer.js
@@ -1,29 +1,54 @@
-const initialState = [];
+// Reducer composition.
 
-const todos = (state = initialState, action) => {
+/**
+ * @param state  - old state of todo
+ * @param action - action that was dispatched
+ * @return new state of todo
+ */
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      // Simply create a object with specified id and text.
+      return {
+        id       : action.id,
+        text     : action.text,
+        completed: false
+      }
+
+    case 'TOGGLE_TODO':
+      // Toggle the completed boolean field only for the specified todo item.
+      if (todo.id != action.id) {
+        return todo;
+      }
+
+      return {
+        ...todo,
+        completed: !todo.completed
+      }
+      
+    default:
+      return state;
+  }
+}
+
+
+/**
+ * @param state  - old state of todos
+ * @param action - action that was dispatched
+ * @return new state of todos
+ */
+const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       // Append the new todo item to the list.
       return [
         ...state,
-        {
-          id       : action.id,
-          text     : action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ];
-    case 'TOGGLE_TODO':
-      // Unless the todo's id matches the id that is specified by action,
-      // return the todo unchanged.
-      // Update the completed boolean only for the specified todo item.
-      return state.map(todo => {
-        if (todo.id != action.id) { return todo; }
 
-        return {
-          ...todo,
-          completed: !todo.completed
-        }
-      });
+    case 'TOGGLE_TODO':
+      return state.map(old => todo(old, action));
+
     default:
       return state;
   }
