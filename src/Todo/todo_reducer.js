362b9356d093f2bd7eb698e8ba95d272a739fd0b
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-12-20 10:51:15 -0500

    Add TOGGLE_TODO to todo-reducer

--- src/Todo/todo_reducer.js
+++ src/Todo/todo_reducer.js
@@ -3,6 +3,7 @@ const initialState = [];
 const todos = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TODO':
+      // Append the new todo item to the list.
       return [
         ...state,
         {
@@ -11,6 +12,18 @@ const todos = (state = initialState, action) => {
           completed: false
         }
       ];
+    case 'TOGGLE_TODO':
+      // Unless the todo's id matches the id that is specified by action,
+      // return the todo unchanged.
+      // Update the completed boolean only for the specified todo item.
+      return state.map(todo => {
+        if (todo.id != action.id) { return todo; }
+
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      });
     default:
       return state;
   }
